140d3751fb7a94c02c6b6112377b4d7427a3b58d
Shiun0413 <a0923183408@gmail.com>
2022-05-15 04:14:18 +0800

    feat: 新增philosophy.php

---
+++ philosophy.php
@@ -0,0 +1,255 @@
+<!DOCTYPE HTML>
+<!--
+	Editorial by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>書籍共享平台</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+
+							<!-- Header -->
+								<header id="header">
+									<a href="index.html" class="logo"><strong>書籍共享平台</strong></a>
+									<ul class="icons">
+										<li><a href="#" class="button primary small">登入</span></a></li>
+									</ul>
+									
+								</header>
+
+							<!-- Banner -->
+							
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>哲學類</h2>
+									</header>
+									<div class="features">
+										<article>
+											<span><img src="images/沉思錄.jpg" alt="" /></span>
+											<div class="content">
+												<h3>沉思錄</h3>
+												<p>作者：馬可·奧理略</p>
+												<p>書籍簡介：影響西方文化兩千年的傳世經典，最完整台灣新譯一代賢帝的立身處世之道，欲成大器者必讀之書<br></p>
+												<ul class="actions">
+													<li><a href="#" class="button">立即借閱</a></li>
+												</ul>
+											</div>
+										</article>
+										<article>
+											<span><img src="images/哲學與人生.jpg" alt="" /></span>
+											<div class="content">
+												<h3>哲學與人生</h3>
+												<p>作者：傅佩榮</p>
+												<p>書籍簡介：「哲學」做為一門學問，原來只是一種生活態度，就是保持好奇的天性，探詢一切事物的真相。這種態度稱為「愛智」。<br></p>
+												<ul class="actions">
+													<li><a href="#" class="button">立即借閱</a></li>
+												</ul>
+											</div>
+										</article>
+										<article>
+											<span><img src="images/廁所裡的哲學課.jpg" alt="" /></span>
+											<div class="content">
+												<h3>廁所裡的哲學課</h3>
+												<p>作者：亞當弗萊徹.盧卡斯‧NP‧艾格.康拉德柯列弗</p>
+												<p>書籍簡介：平均來說，我們每天會花14分鐘蹲馬桶，既然如此，何不善用蹲馬桶的寶貴時間，學點真正有用的東西？<br></p>
+												<ul class="actions">
+													<li><a href="#" class="button">立即借閱</a></li>
+												</ul>
+											</div>
+										</article>
+										<article>
+											<span><img src="images/西方哲學與人生.jpg" alt="" /></span>
+											<div class="content">
+												<h3>西方哲學與人生</h3>
+												<p>作者：亞當弗萊徹</p>
+												<p>書籍簡介：哲學就是人生經濟學，用最少時間，達成最大效果。<br></p>
+												<ul class="actions">
+													<li><a href="#" class="button">立即借閱</a></li>
+												</ul>
+											</div>
+										</article>
+									</div>
+								</section>
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>Ipsum sed dolor</h2>
+									</header>
+									<div class="content">
+										<article>
+											<a href="#" class="image"><img src="images/灰階思考.jpg" alt="" /></a>
+											<h3>Interdum aenean</h3>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+											<ul class="actions">
+												<li><a href="#" class="button">More</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/灰階思考.jpg" alt="" /></a>
+											<h3>Nulla amet dolore</h3>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+											<ul class="actions">
+												<li><a href="#" class="button">More</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/灰階思考.jpg" alt="" /></a>
+											<h3>Tempus ullamcorper</h3>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+											<ul class="actions">
+												<li><a href="#" class="button">More</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/灰階思考.jpg" alt="" /></a>
+											<h3>Sed etiam facilis</h3>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+											<ul class="actions">
+												<li><a href="#" class="button">More</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/灰階思考.jpg" alt="" /></a>
+											<h3>Feugiat lorem aenean</h3>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+											<ul class="actions">
+												<li><a href="#" class="button">More</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/灰階思考.jpg" alt="" /></a>
+											<h3>Amet varius aliquam</h3>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+											<ul class="actions">
+												<li><a href="#" class="button">More</a></li>
+											</ul>
+										</article>
+									</div>
+								</section>
+
+						</div>
+					</div>
+
+				<!-- Sidebar -->
+					<div id="sidebar">
+						<div class="inner">
+
+							<!-- Search -->
+								<section id="search" class="alt">
+									<form method="post" action="#">
+										<input type="text" name="query" id="query" placeholder="Search" />
+									</form>
+								</section>
+
+							<!-- Menu -->
+								<nav id="menu">
+									<header class="major">
+										<h2>Menu</h2>
+									</header>
+									<ul>
+										<li><a href="index.html">首頁</a></li>
+										<li><a href="generic.html">借閱書籍</a></li>
+										<li><a href="elements.html">分享書籍</a></li>
+										<li>
+											<span class="opener">瀏覽書籍</span>
+											<ul>
+												<li><a href="#">哲學類</a></li>
+												<li><a href="#">宗教類</a></li>
+												<li><a href="#">科學類</a></li>
+												<li><a href="#">應用科學類</a></li>
+												<li><a href="#">社會科學類</a></li>
+												<li><a href="#">史地類：中國史地</a></li>
+												<li><a href="#">史地類：世界史地</a></li>
+												<li><a href="#">語言文學類</a></li>
+												<li><a href="#">藝術類</a></li>
+											</ul>
+										</li>
+										<li><a href="#">Etiam Dolore</a></li>
+										<li><a href="#">Adipiscing</a></li>
+										<li>
+											<span class="opener">Another Submenu</span>
+											<ul>
+												<li><a href="#">Lorem Dolor</a></li>
+												<li><a href="#">Ipsum Adipiscing</a></li>
+												<li><a href="#">Tempus Magna</a></li>
+												<li><a href="#">Feugiat Veroeros</a></li>
+											</ul>
+										</li>
+										<li><a href="#">Maximus Erat</a></li>
+										<li><a href="#">Sapien Mauris</a></li>
+										<li><a href="#">Amet Lacinia</a></li>
+									</ul>
+								</nav>
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>Ante interdum</h2>
+									</header>
+									<div class="mini-posts">
+										<article>
+											<a href="#" class="image"><img src="images/pic07.jpg" alt="" /></a>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic08.jpg" alt="" /></a>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic09.jpg" alt="" /></a>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>
+										</article>
+									</div>
+									<ul class="actions">
+										<li><a href="#" class="button">More</a></li>
+									</ul>
+								</section>
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>Get in touch</h2>
+									</header>
+									<p>Sed varius enim lorem ullamcorper dolore aliquam aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin sed aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+									<ul class="contact">
+										<li class="icon solid fa-envelope"><a href="#">information@untitled.tld</a></li>
+										<li class="icon solid fa-phone">(000) 000-0000</li>
+										<li class="icon solid fa-home">1234 Somewhere Road #8254<br />
+										Nashville, TN 00000-0000</li>
+									</ul>
+								</section>
+
+							<!-- Footer -->
+								<footer id="footer">
+									<p class="copyright">&copy; Untitled. All rights reserved. Demo Images: <a href="https://unsplash.com">Unsplash</a>. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+								</footer>
+
+						</div>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
