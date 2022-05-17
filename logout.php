a17d8665487ef22189f2d5df1d0fcf38b99f268b
Shiun0413 <a0923183408@gmail.com>
2022-05-17 22:09:55 +0800

    fix: fixed #3

--- logout.php
+++ logout.php
@@ -2,6 +2,7 @@
     session_start();
     unset($_SESSION["account"]);
     unset($_SESSION["name"]);
+    unset($_SESSION["name"]);
     
     header('location:login.php?method=message&message=已登出');
 ?>
