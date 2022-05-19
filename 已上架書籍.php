5115746ae0e9cd224f31cb4e017301b20695c0d1
Alvin59567 <104577241+Alvin59567@users.noreply.github.com>
2022-05-19 20:13:15 +0800

    Add files via upload
    更新下架按鈕，使其連接到後端刪除的.php

--- 已上架書籍.php
+++ 已上架書籍.php
@@ -69,7 +69,7 @@ $name=$_SESSION['name'];
 
 
                             <ul class="actions">
-                                <li><a href="#" class="button">下架</a></li>
+                                <li><a href="下架書籍.php?book_id=<?php echo $rslt['book_id'] ?>" class="button">下架</a></li>
                             </ul>
                         </article>
                         <?php }?>
