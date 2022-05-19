503bafdfa692e5ee4391745a4e74fe92c6b6f18d
pinyu01 <104435602+pinyu01@users.noreply.github.com>
2022-05-19 22:32:05 +0800

    Update search.php

--- search.php
+++ search.php
@@ -68,15 +68,14 @@
                     <p align="center"><B>關鍵詞搜索結果如下：</B></p>
                     <?php if($totalRows_rs>0) do { ?>
                         <div class="book_jpg_style123">
-                            <img src="images/<?php echo $row_rs['book_image'];?>" />
+                        <a href="書籍內容.php?book=<?php echo $rslt['book_name'] ?>" class="image"><img src='images/<?php echo $row_rs['book_image'];?>'/></a>
                         </div>
-                    <p>書名 : <?php echo $row_rs["book_name"]; ?><br></p>
-                    <p>作者 : <?php echo $row_rs["book_author"]; ?><br></p>
-                    <p>出版社 : <?php echo $row_rs["public"]; ?><br></p>
-                    <p>出版日期 : <?php echo $row_rs["public_date"]; ?><br></p>
-                    <p>類別 : <?php echo $row_rs["book_category"]; ?><br></p>
-                    <p>簡介 : <?php echo $row_rs["book_introduction"]; ?><br></p>
-                    <br><br><br><br>
+                        <h3>書名 :<?php echo $row_rs["book_name"]; ?></h3>
+                        <p>作者 : <?php echo $row_rs["book_author"]; ?><br></p>
+                        <p>出版社 : <?php echo $row_rs["public"]; ?><br></p>
+                        <p>出版日期 : <?php echo $row_rs["public_date"]; ?><br></p>
+                        <p>類別 : <?php echo $row_rs["book_category"]; ?><br></p>
+                        <br><br><br><br>
                     <?php } while ($row_rs = mysqli_fetch_assoc($rs)); ?>
                     
 
